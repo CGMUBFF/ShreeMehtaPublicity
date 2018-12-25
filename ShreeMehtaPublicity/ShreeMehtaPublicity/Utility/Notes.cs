ccbbfde8fbfe44d9059475b103b7cd1600efb5e3
Mohit Sanghavi <mohit.sanghvi@streebo.com>
2018-12-25 17:08:21 +0530

    Mail Sent

---
+++ ShreeMehtaPublicity/ShreeMehtaPublicity/Utility/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShreeMehtaPublicity.Utility
+{
+    class Notes
+    {
+        /*
+        General :
+        1. User responses for Add / Modify / Save / etc. operations
+
+        Cautation :
+        1. 
+
+        */
+    }
+}
